34f0b529d8db59d33870f36a1494cf8d442991c3
Lorenzoo2208 <baiginilorenzo22@gmail.com>
2024-12-11 13:02:01 +0100

    aaa

---
+++ Lezione zaza/Lezioneza.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+    //sizeoff(type)
+
+    int dim_int;
+    dim_int = sizeof(int);
+
+    float test=0.4;
+
+    printf("il valore in termini di byte di int e': %d\n",&dim_int,test);
+
+    return 0;
+                                                                                                                                                                                                                                                                                                      
+}
