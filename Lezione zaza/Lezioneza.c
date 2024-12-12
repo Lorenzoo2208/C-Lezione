e1d32826e88eaef4e43aa137d053529d3a48950d
Lorenzoo2208 <baiginilorenzo22@gmail.com>
2024-12-12 13:09:12 +0100

    aaaa

--- Lezione zaza/Lezioneza.c
+++ Lezione zaza/Lezioneza.c
@@ -1,16 +1,156 @@
 #include <stdio.h>
 
-int main ()
-{
-    //sizeoff(type)
+// int main ()
+// {
+//     //sizeoff(type)
 
-    int dim_int;
-    dim_int = sizeof(int);
+//     int dim_int;
+//     dim_int = sizeof(int);
 
-    float test=0.4;
+//     float test=0.4;
 
-    printf("il valore in termini di byte di int e': %d\n",&dim_int,test);
+//     printf("il valore in termini di byte di int e': %d\n",&dim_int,test);
 
-    return 0;
+//     return 0;
                                                                                                                                                                                                                                                                                                       
-}
+// }
+
+
+//  int main() 
+//  {
+//     int i = 5;
+//     float f = 3.7;
+//     int iSomma = i + f;
+//     float fSomma = i + f;
+//     printf("La somma intera di %d e f e' %d\n", i, f, iSomma);
+//     printf("Conversione esplicita a float: %f\n", (float)iSomma);
+//     printf("La somma float di %d e %f e' %f\n", i, f, fSomma);
+//     return 0;
+
+//  }
+
+
+//   int main()
+  
+//   {
+//     float i = 5.75
+//     int f =(int) i;
+
+//     printf("il valore float: %f.2f\n ", valore,float;
+//     printf("Valore int: %d\n", valore_int);
+
+//     return 0;
+
+//  }
+
+//   int main() 
+//  {
+//     int i = 5;
+//     float f = 3.7;
+//     int f =(int) i;
+//     int iSomma = i + f;
+//     float fSomma = i + f;
+
+//     printf("il valore float: %f.2f\n ", valore,float;
+//     printf("Valore int: %d\n", valore_int);
+//     printf("La somma intera di %d e f e' %d\n")
+//     scanf(%d,&i)
+//     printf("Conversione esplicita a float: %f\n");
+//     printf("La somma float di %d e %f e' %f\n";
+//     return 0;
+//     da finire
+
+//  }
+
+    // int main ()
+    // {
+    //    int settanta = 70;
+    //    char e = "E";
+    //    printf("L'intero %d corrisponde al carattere %d, e")
+    // }
+
+    // int main()
+    // {
+    //     float P,R,T,SI;
+    //     printf("Inserire capitale, tasso, tempo:");
+    //     scanf("%f, %f, %f, &P, &R, &T");
+    //     SI = (P * R * T) / 100;
+    //     printf("Interesse semplice: %.2f\n", SI);
+    //     return 0;
+
+
+    // }
+
+    // int main()
+    
+    // {
+    //     int a ;
+
+    //     printf("inserisci il valore")
+    //     scanf("%d,&a")
+    //     if (a % 2 == 0)
+    
+    //     {
+    //         printf(" il numero è pari", a)
+    //          if(a % 5 === 0)
+    //     {
+    //         printf("il numero è divisibile per 5\n", a)
+
+
+    //     } 
+    //     }
+        
+       
+
+    //     else
+    //     printf("il numero inserito è dispari", a)
+
+    // }
+
+
+
+    // i primi n multipli di k
+
+    // int main()
+    // {
+    //     int risultato;
+    //     int n;
+    //     int k;
+    //     int i = 1;
+
+    //     printf("inserisci il valore di n\n");
+    //     scanf("%d,&n");
+
+
+    //     printf("inserisci il valore di k\n");
+    //     scanf("%d,&k");
+
+    //     for (i=1; !<=n; i++)
+    //         printf("Il multiplo %d di %d e' %d\n",i, k, i*k);
+
+    //     {
+
+
+    //     }
+
+    // }
+
+
+    int main()
+    {
+
+        int N, somma = 0;
+
+        printf("Inserisci un numero: ");
+        scanf("%d", &N);
+
+        for ( int i = 1; i <= N; i++)
+        {
+            somma += 1;
+        }
+        printf("La somma dei primi %d numeri e' %d\n", N ,somma)
+        ;
+
+        return 0;
+    }
+
