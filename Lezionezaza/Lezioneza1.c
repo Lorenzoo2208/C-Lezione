0b956b027da379ddbb575925f4c50542cc231bc1
Lorenzoo2208 <baiginilorenzo22@gmail.com>
2025-01-08 13:08:41 +0100

    adada

---
+++ Lezionezaza/Lezioneza1.c
@@ -0,0 +1,424 @@
+#include <stdio.h>
+
+// int main ()
+// {
+//     //sizeoff(type)
+
+//     int dim_int;
+//     dim_int = sizeof(int);
+
+//     float test=0.4;
+
+//     printf("il valore in termini di byte di int e': %d\n",&dim_int,test);
+
+//     return 0;
+
+// }
+
+
+//  int main()
+//  {
+//     int i = 5;
+//     float f = 3.7;
+//     int iSomma = i + f;
+//     float fSomma = i + f;
+//     printf("La somma intera di %d e f e' %d\n", i, f, iSomma);
+//     printf("Conversione esplicita a float: %f\n", (float)iSomma);
+//     printf("La somma float di %d e %f e' %f\n", i, f, fSomma);
+//     return 0;
+
+//  }
+
+
+//   int main()
+
+//   {
+//     float i = 5.75
+//     int f =(int) i;
+
+//     printf("il valore float: %f.2f\n ", valore,float;
+//     printf("Valore int: %d\n", valore_int);
+
+//     return 0;
+
+//  }
+
+//   int main()
+//  {
+//     int i = 5;
+//     float f = 3.7;
+//     int f =(int) i;
+//     int iSomma = i + f;
+//     float fSomma = i + f;
+
+//     printf("il valore float: %f.2f\n ", valore,float;
+//     printf("Valore int: %d\n", valore_int);
+//     printf("La somma intera di %d e f e' %d\n")
+//     scanf(%d,&i)
+//     printf("Conversione esplicita a float: %f\n");
+//     printf("La somma float di %d e %f e' %f\n";
+//     return 0;
+//     da finire
+
+//  }
+
+    // int main ()
+    // {
+    //    int settanta = 70;
+    //    char e = "E";
+    //    printf("L'intero %d corrisponde al carattere %d, e")
+    // }
+
+    // int main()
+    // {
+    //     float P,R,T,SI;
+    //     printf("Inserire capitale, tasso, tempo:");
+    //     scanf("%f, %f, %f, &P, &R, &T");
+    //     SI = (P * R * T) / 100;
+    //     printf("Interesse semplice: %.2f\n", SI);
+    //     return 0;
+
+
+    // }for (int i =0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+
+//             matrix [i][j] = value++;
+//         }
+//     }
+//     //     if (a % 2 == 0)
+// for (int i =0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+
+//             matrix [i][j] = value++;
+//         }
+//     }
+
+
+    //     else
+    //     printf("il numero inserito è dispari", a)
+
+    // }
+
+
+
+    // i primi n multipli di k
+
+    // int main()
+    // {
+    //     int risultato;
+    //     int n;
+    //     int k;
+    //     int i = 1;
+
+    //     printf("inserisci il valore di n\n");
+    //     scanf("%d,&n");
+
+
+    //     printf("inserisci il valore di k\n");
+    //     scanf("%d,&k");
+
+    //     for (i=1; !<=n; i++)
+    //         printf("Il multiplo %d di %d e' %d\n",i, k, i*k);
+
+    //     {
+
+
+    //     }
+
+    // }
+
+
+    // int main()
+    // {
+
+    //     int N, somma = 0;
+
+    //     printf("Inserisci un numero: ");
+    //     scanf("%d", &N);
+
+    //     for ( int i = 1; i <= N; i++)
+    //     {
+    //         somma += 1;
+    //     }
+    //     printf("La somma dei primi %d numeri e' %d\n", N ,somma)
+    //     ;
+
+    //     return 0;
+    // }
+
+    // convertire un numero decimale in numero binario uasndo le divisioni
+
+    // int main()
+    // {
+    //     void decimaleTobinario(int numero){for (int i =0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+
+    //         matrix [i][j] = value++;
+    //     }
+    // }
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+
+    //         matrix [i][j] = value++;
+    //     }
+    // }
+    // }
+
+// #include <stdio.h>
+// #include <math.h>
+
+// int main ()
+// {
+
+//     float numero;
+//     float sinNumero;
+//     printf("Inserisci un numero: ");
+//     scanf("%f", &numero);
+//     sinNumero = sin(numero);
+
+//     printf("sin (%.2f) = %.2f\n", numero, sinNumero);
+//     printf("cos (%.2f) = %.2f\n", numero, cos(numero));
+//     printf("|%.2f| = %.2f\n", numero, fabs(numero));
+//     printf("Intero Inferiore: %.0f\n", floor(numero));
+//     printf("Intero Superiore: %.0f\n", ceil(numero));
+//     return 0;
+// }
+
+
+// calcolare e stamapare la radice quadrata  di un numero letto da tastiera. In caso venga messo un numero negativo stampare messaggio di errore.
+
+
+// #include <stdio.h>
+// #include <math.h>
+
+// int main ()
+// {
+//     double numero, radice;
+
+    
+//     printf("Inserisci un numero: ");
+//     scanf("%lf", &numero);
+    
+//     if (numero < 0) 
+//     {
+//         printf("Errore non è possibile calcolare il quadrato.\n");
+
+//     }
+
+//     else 
+    
+//     {
+
+//         double radice = sqrt (numero);
+//         printf("La radice quadrata di %.2lf è %.2lf\n", numero , radice);
+//     }
+
+//     return 0;
+// }
+
+// leggere 10 valori da tastiera memorizzarli in un vettore; quindi calcolare il minimo è il massimo
+
+// int main()
+// {
+// int valori = 10;
+// int minimo, massimo;
+    // int i;
+
+// printf=("inserisci 10 numeri interi\n");
+// for (int i=0; i < 10; i++)
+// {
+// printf(" valore %d:", i + 1);
+// scanf("%d", &valori[i]);
+
+// minimo = massimo = valori[0];
+
+// for (int i = 1; i< 10; i++)
+// {
+//     if (valori[i] < minimo)
+//     minimo = valori[i];
+// // }
+// // if (valori[i] > massimo)
+// // {
+// //     massimo = valori[i];for (int i =0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+
+//             matrix [i][j] = value++;
+//         }
+//     }
+// //     int valori = 5;
+// //     int k = valori;for (int i =0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+
+//             matrix [i][j] = value++;
+//         }
+//     }
+// // int main()
+// // {
+
+
+// // }
+
+// // dichiarare una matrice 3x3 e riempirla con i numeri da 1 a 9
+
+// int main()
+// {
+//     int matrix [3][3];
+//     int value = 1;
+//     for (int i =0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+
+//             matrix [i][j] = value++;
+//         }
+//     }
+
+
+//     for (int i =0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+
+//             printf("L'elemento [%d]")
+//         }
+//     }
+//     printf("%d, &matrix");
+
+//     return 0;
+// }
+
+// data una parola letta come stringa s, stabilire se s è palindroma(ossia se è simmetrica)
+
+// #include <string.h>
+
+// int main()
+// {
+
+//     char s[100];
+//     int palindroma = 1;
+
+//     printf("inserisci una parola:");
+//     scanf("%s", s);
+
+//     int lunghezza = strlen(s);
+
+//     for (int i = 0; i < lunghezza / 2; i++)
+//     {
+
+//         if (s[i] != s[lunghezza - i -1])
+//         {
+
+//             palindroma = 0;
+//             break;
+//         }
+//     }
+//     if(palindroma)
+//     {
+//         printf("La parola \"%s\" e' palindroma.\n", s);
+
+//     }
+//     else
+//     {
+//         printf("La parola \"%s\" non e' palindroma.\n");
+//     }
+// }
+
+// Esempio uso puntatori
+
+// int main ()
+// {
+
+//     int a;
+//     int *p;
+
+//     a=5;
+//     printf("A ha indirizzo %p e valore %d\n",&a,a);
+//     p=&a;
+//     printf("p ha indirizzo %p e valore %p\n",&p,p);
+//     *p=6;
+//     printf ("a vale %d\n",a);
+
+//     return 0;
+// }
+
+// scrivere un programma che: chiede all'utente di riempire un matrice 3x4,
+// stampi la matrice, stampi la posizione della prima occorenza di 0 se esiste.
+
+// int main() {
+//     int matrice[3][4];
+//     int i, j;
+//     int trovato = 0;
+
+//     // Raccogliamo i dati dall'utente
+//     printf("Inserisci i valori per la matrice 3x4:\n");
+//     for (i = 0; i < 3; i++) {
+//         for (j = 0; j < 4; j++) {
+//             printf("Inserisci il valore per la posizione (%d,%d): ", i + 1, j + 1);
+//             scanf("%d", &matrice[i][j]);
+//         }
+//     }
+
+//     // Stampiamo la matrice
+//     printf("\nMatrice inserita:\n");
+//     for (i = 0; i < 3; i++) {
+//         for (j = 0; j < 4; j++) {
+//             printf("%d ", matrice[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     // Cerchiamo la prima occorrenza di 0
+//     for (i = 0; i < 3; i++) {
+//         for (j = 0; j < 4; j++) {
+//             if (matrice[i][j] == 0) {
+//                 printf("\nLa prima occorrenza di 0 è nella posizione (%d,%d)\n", i + 1, j + 1);
+//                 trovato = 1;
+//                 break;
+//             }
+//         }
+//         if (trovato) {
+//             break;
+//         }
+//     }
+
+//     // Se non è stato trovato nessun 0
+//     if (!trovato) {
+//         printf("\nNon è presente alcun 0 nella matrice.\n");
+//     }
+
+//     return 0;
+// }
+
+
+void fibonacci (int = l);
+
+int main()
+{
+
+int n;
+printf("metti un numero: ");
+scanf("%d, l");
+fibonacci(l);
+
+
+
+
+
+return = 0;
+}
+
+void fibonacci(int l)
+{
+
+}
---
+++ Lezionezaza/Lezionezaza2.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <math,h>
+#include <stdlib.h>
+#include <string.h>
+
+// void doppioVal (int a){
+//     a=a*2;
+// }
+
+// void doppioind (int *a){
+
+//     *a=*a*2;
+// }
+
+// int main()
+// {
+//     int n=5;
+//     printf("%d\n", n);
+
+//     doppioVal (n);
+//     printf("%d\n" ,n);
+
+//     doppioind(&n);
+//     printf("%d\n", n);
+
+//     return 0;
+     
+// }
+
+
+// 
+
+// int main(int argc, char **argv)
+// {
+//     int iArh;
+//     for (iArg=0; iArg<argc; iArg++)
+//     {
+//         printf("Parametro %d: %s\n", iArg, argv[iArg]);
+
+//     }
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     float a,b,risultato;
+
+//     printf("Inserisci il primo operando\n");
+//     scanf("%f,&a");
+//     printf("Inserisci il secondo operando\n");
+//     scanf("%f,&b");
+//     printf("leggi l'operando\n");
+//     scanf("%d,&operazione");
+
+//     if (strcmp(operazione,"moltiplicazione") == 0)
+//     {
+
+//         risultato = a * b;
+//     }
+
+
+//     else if (strcmp(operazione,"divisione") == 0)
+//     {
+
+//         risultato = a * b;
+//     }
+
+//     else if (strcmp(operazione,"diminuisci") == 0)
+//     {
+
+//         risultato = a - b;
+//     }
+
+
+//     else if (strcmp(operazione,"sottrazione") == 0)
+//     {
+
+//         risultato = a + b;
+//     }
+
+
+
+//     switch(operazione)
+//     {
+//         case 0;
+//         risultato =a*b;
+//         break;
+
+//         case 1;
+//         risultato =a/b;
+//         break;
+
+//         case 2;
+//         risultato =a+b;
+//         break;
+
+//         case 3;
+//         risultato =a-b;
+//         break;
+
+
+//     }
+
+//     printf(" Risultato e %f", risultato);
+
+//     return 0;
+// }
+
+
+    
